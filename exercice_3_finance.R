3c7c4b37e8691a7cc54936163c4721d76bb2c74c
dagboelisee <dagboelisee7@gmail.com>
2026-04-15 16:54:59 +0200

    Q3-5 fonctions de base

--- exercice_3_finance.R
+++ exercice_3_finance.R
@@ -18,3 +18,41 @@ df_m_2018_01 <- df_fin %>% filter(Month == "2018-01")
 df_m_2018_02 <- df_fin %>% filter(Month == "2018-02")
 
 
+# Q3 : Fonction de filtrage
+filtrer_depenses <- function(df) {
+  stopifnot(is.data.frame(df)) # Vérification de l'argument [7]
+
+  df_filtre <- df %>% filter(`Transaction Type` == "debit")
+  return(df_filtre)
+}
+
+# Test : Combien de lignes sont filtrées ?
+nrow(df_fin) - nrow(filtrer_depenses(df_fin)) #118
+
+# Q4 : Fonction pour compter le nombre de transactions
+compter_transactions <- function(df) {
+  stopifnot(is.data.frame(df))
+  return(nrow(df))
+}
+
+# Q5 : Fonction pour compter le nombre de catégories uniques (équivalent aux "boucles" [8])
+compter_categories <- function(df) {
+  stopifnot(is.data.frame(df))
+  return(length(unique(df$Category)))
+}
+
+platinum_depenses <- filtrer_depenses(df_a_platinum)
+silver_depenses <- filtrer_depenses(df_a_silver)
+janvier_depenses <- filtrer_depenses(df_m_2018_01)
+fevrier_depenses <- filtrer_depenses(df_m_2018_02)
+
+compter_transactions(df_a_platinum)
+compter_transactions(df_a_silver)
+compter_transactions(df_m_2018_01)
+compter_transactions(df_m_2018_02)
+
+compter_categories(df_a_platinum)
+compter_categories(df_a_silver)
+compter_categories(df_m_2018_01)
+compter_categories(df_m_2018_02)
+
